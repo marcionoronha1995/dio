1e25aadc3e17f48b67f1c4aab3b487581c5a4d5d
marcionoronha1995 <123603516+marcionoronha1995@users.noreply.github.com>
2023-10-14 11:08:58 -0300

    MinhaClasse.java atu

--- Formacao-java-developer/java-anatomia-classes/src/MinhaClasse.java
+++ Formacao-java-developer/java-anatomia-classes/src/MinhaClasse.java
@@ -1,5 +1,21 @@
 public class MinhaClasse {
+    /**
+     * @param args
+     */
     public static void main (String [] args) {
-        System.out.print ("Olá Turma! Sejam bem vindos");
+        System.out.print ("Olá Turma! Variáveis");
+        String meuNome = "Marcio";
+        int anoNascimento = 1969;
+        boolean simNao = true;
+        double altura = 1.62;
+        Dog spike;
+        int somar (int numeroUm, int numeroDois);
+        String formatar (long cep);
+
+
+        anoNascimento = 2023;
+        simNao = false;
+
+
     } 
 }
