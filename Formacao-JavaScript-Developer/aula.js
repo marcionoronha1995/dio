29423f638f9dad7356798893083f67f56f3112f0
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-12-11 08:18:13 -0300

    condicoes true false

--- Formacao-JavaScript-Developer/aula.js
+++ Formacao-JavaScript-Developer/aula.js
@@ -1,3 +1,8 @@
 console.log("....novo teste")
-const camisaMarcio = false;
-const camisaMarleny = false;
+const camisaMarcio = "azul";
+const camisaMarleny = "amarelo";
+console.log(camisaMarcio.length);
+console.log(camisaMarleny.length);
+console.log(camisaMarcio.length + camisaMarleny.length);
+console.log((camisaMarcio.length % 2) === 0 );
+console.log((camisaMarleny.length % 2) === 0);
