ca98ef6c517e2edf9e13bf175de3e9f701e0baf5
marcionoronha1995 <marcionoronha1995@gmail.com>
2023-12-31 10:19:57 -0300

    if else boleam

--- Formacao-JavaScript-Developer/aula.js
+++ Formacao-JavaScript-Developer/aula.js
@@ -6,3 +6,20 @@ console.log(camisaMarleny.length);
 console.log(camisaMarcio.length + camisaMarleny.length);
 console.log((camisaMarcio.length % 2) === 0 );
 console.log((camisaMarleny.length % 2) === 0);
+if ((camisaMarcio.length % 2) === 0) {
+ console.log(camisaMarcio.length + " o número de caracteres da string é par");
+} else {
+    console.log(camisaMarcio.length + "é impar");
+};
+if ((camisaMarleny.length % 2) === 0) {
+ console.log(camisaMarleny.length + " o número de caracteres da string é par");
+} else {
+    console.log(camisaMarleny.length + " o número de caracteres da string é impar");
+};
+const numeroZero = 0;
+console.log(numeroZero);
+if ((numeroZero % 2) === 0) {
+ console.log(numeroZero + " o número da constante é par");
+} else {
+    console.log(numeroZero + "é impar");
+};
